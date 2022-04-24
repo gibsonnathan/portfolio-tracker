1beec3750a87b85cc53a5a9baa984f2a6937d3dc
nathan <gibson_nathaniel1@columbusstate.edu>
2022-04-24 19:10:56 -0400

    added implementation for getStockAtInstant

--- src/main/java/com/nathangibson/portfolio/service/StockHistoryService.java
+++ src/main/java/com/nathangibson/portfolio/service/StockHistoryService.java
@@ -7,7 +7,10 @@ import com.nathangibson.portfolio.repository.StockHistoryRepository;
 import com.nathangibson.portfolio.repository.StockRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class StockHistoryService {
@@ -25,5 +28,33 @@ public class StockHistoryService {
     StockEntity stockEntity = stockRepository.findByTicker(ticker)
         .orElseThrow(() -> new StockNotFoundException());
     return stockHistoryRepository.findByStockId(stockEntity.getId());
+  }
+
+  public Optional<StockHistoryEntity> getStockAtInstant(String ticker,
+                                                        Instant instant) {
+    StockEntity stockEntity = stockRepository.findByTicker(ticker)
+        .orElseThrow(() -> new StockNotFoundException());
+    List<StockHistoryEntity> stockHistoryEntities =
+        stockHistoryRepository.findByStockId(stockEntity.getId()).stream()
+            .sorted((x, y) -> {
+              if (x.equals(y)) {
+                return 0;
+              } else if (x.getTimestamp().isAfter(y.getTimestamp())) {
+                return 1;
+              } else {
+                return -1;
+              }
+            }).collect(Collectors.toList());
+
+    for (int i = 1; i < stockHistoryEntities.size(); i++) {
+      StockHistoryEntity previous = stockHistoryEntities.get(i - 1);
+      StockHistoryEntity current = stockHistoryEntities.get(i);
+      if (instant.isAfter(previous.getTimestamp()) &&
+          current.getTimestamp().isAfter(instant)) {
+        return Optional.of(current);
+      }
+    }
+
+    return Optional.empty();
   }
 }
